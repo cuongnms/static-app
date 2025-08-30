0338ec667687c0ca2e797a2180b89ba021dadf67
CuongNMs <nguyen.minh.cuong.1102@gmail.com>
2025-08-30 23:21:44 +0700

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Test CI/CD</h1>
+      <h1>Is OK</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
